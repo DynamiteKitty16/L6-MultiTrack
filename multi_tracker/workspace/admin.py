c285a6e3122d5188673f3f4b993b3a3886d2b58f
DynamiteKitty16 <katieboal16@hotmail.co.uk>
2024-11-06 13:53:08 +0000

    registered models in admin

--- multi_tracker/workspace/admin.py
+++ multi_tracker/workspace/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserProfile, AttendanceRecord, LeaveRequest
 
-# Register your models here.
+# Registering the models for django interface
+admin.site.register(UserProfile)
+admin.site.register(AttendanceRecord)
+admin.site.register(LeaveRequest)
