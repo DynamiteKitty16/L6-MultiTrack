160a72993ba63513b70ae0032958aef937fa88e6
DynamiteKitty16 <katieboal16@hotmail.co.uk>
2024-11-13 17:26:06 +0000

    commented out urls for testing

---
+++ multi_tracker/multi_tracker/tenant_urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+# Placeholder view function
+def tenant_view(request):
+    pass
+
+urlpatterns = [
+    
+]
--- multi_tracker/multi_tracker/urls.py
+++ multi_tracker/multi_tracker/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),  # Django's built-in auth views
 
     # Modular URLs
-    path('tenant/', include('multi_tracker.tenant_urls')),         # Tenant admin-specific views
-    path('manager/', include('multi_tracker.manager_urls')),       # Manager-specific views
-    path('', include('multi_tracker.user_urls')),                  # Regular user views (home, login, etc.)
+    #path('tenant/', include('multi_tracker.tenant_urls')),         # Tenant admin-specific views
+    #path('manager/', include('multi_tracker.manager_urls')),       # Manager-specific views
+    #path('', include('multi_tracker.user_urls')),                  # Regular user views (home, login, etc.)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
