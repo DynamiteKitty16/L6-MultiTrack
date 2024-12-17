aadfcdf786c350e44e61e839a4a1096b7110d6ab
DynamiteKitty16 <katieboal16@hotmail.co.uk>
2024-12-17 01:08:01 +0000

    Add psycopg2-binary to requirements

--- multi_tracker/multi_tracker/settings.py
+++ multi_tracker/multi_tracker/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ENVIRONMENT VARIABLES
 # Load SECRET_KEY securely from environment variables or .env file
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', config('DJANGO_SECRET_KEY', default='fallback-insecure-key'))
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
 # Email configuration
